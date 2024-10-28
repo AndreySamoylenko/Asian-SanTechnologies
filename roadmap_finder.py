8293bc1527251277889365bf0f46a77ee3a0667b
EndForced <evafagsclub@gmail.com>
2024-10-28 11:08:33 +1000

    Update roadmap_finder.py

--- roadmap_finder.py
+++ roadmap_finder.py
@@ -4,17 +4,18 @@ tubes_cords = [[(2,4),1],[(3,1),2],[(5,6),1]] #cords / floor
 field_matrix = [
                [0,1,0,0,0,0,0,0], #possible values: 0 - empty
                [0,0,1,1,1,0,0,0],  #1 - second floor module, 2x - ramp, can be 23 24 25 26, last symbol means direction
-               [0,0,1,1,1,0,0,0],  #3x up-down ramps, can be 37,38 - last symbols - also direction
-               [0,0,0,0,0,1,1,1],
+               [0,0,1,1,1,0,32,0],  #3x up-down ramps, can be 37,38 - last symbols - also direction
+               [0,0,0,35,0,1,1,1],
                [1,1,0,0,0,0,0,0],
                [0,0,0,0,1,0,34,1],
                [1,0,0,0,0,0,0,0],
-               [0,0,0,0,0,0,0,1]
+               [0,0,0,0,0,0,0,0]
                 ]
 
 def unreachable_sec_floor_segments_detection(mat):
     dead_cells = []
     cells_lifesavers = []
+    maybe_cells = []
     for i in range(0,len(mat)):
         for j in range(0,len(mat[i])):
             if mat[j][i] == 1:
@@ -25,9 +26,19 @@ def unreachable_sec_floor_segments_detection(mat):
                     cells_lifesavers.append((j,i))
 
                 elif vector_to_nearest(1,(j,i),mat)[0] <= 1:
-                    pass #по идее надо выявлять 2 этажи которые стоят около рамп и искать связь с такими секторами но мне лень
+                    maybe_cells.append((j,i))
 
-    return dead_cells
+    for i in range(100):
+        for i in cells_lifesavers:
+            for j in maybe_cells:
+                if math.dist(i,j) == 1 and math.dist(i,j) != 0:
+                    cells_lifesavers.append(j)
+                    maybe_cells.remove(j)
+
+    for i in range(len(maybe_cells)):
+        dead_cells.append(maybe_cells[i])
+
+    return dead_cells,cells_lifesavers
 def vector_to_nearest(type,cords,mat):
     objects_list = find_coordinates(mat,type)
     min_dist = 50
@@ -45,7 +56,5 @@ def find_coordinates(mat, obj):
                 coordinates.append((i, j))
     return coordinates
 
-
 print(unreachable_sec_floor_segments_detection(field_matrix))
 
-#print(vector_to_nearest(3,(7,7),field_matrix))
