1bc895a189d596603edc665cb5389227572a114e
EndForced <evafagsclub@gmail.com>
2025-03-23 19:43:17 +1000

    commit

--- testing/path_finding_1.py
+++ testing/path_finding_1.py
@@ -4,11 +4,11 @@
 
 (возможно добавим краям веса)
 """
-import time
+from emulator import *
 import numpy as np
-from emulator import Emulator
+from Future_engeneers_path_creation_new import *
 
-em = Emulator()
+em = emulator
 field_mat = np.array([
     [42, 10, 10, 10, 10, 10, 10, 10],
     [20, 33, 20, 20, 20, 34, 10, 33],
@@ -56,7 +56,38 @@ def interest_calculation(field_mat, coef_mat):
 
     return result
 
+def border_find(interest,mat,pos,dir):
+    revealed = np.array([[0 if cell == 0 else 1 for cell in row] for row in field_mat])
+    cords = {}
+    for i in range(len(field_mat)):
+        for j in range(len(field_mat)):
+            cords[(i,j)] = revealed[i][j]*interest[i][j]
+    way = []
+    while not way:
+        to_go = max(cords.values())
+        for key, val in cords.items():
+            if val == to_go:
+                to_go = key
+                break
+        con_dict = neighbour_ini(replace_ints_in_matrix(mat))
+        waves = wave_ini(pos,con_dict)
+        way = wave_back_way(waves,pos,to_go,con_dict,0,None,mat)
+        if way:
+            way = way_to_commands([way,0],field_mat)
+            print("way")
+        else:
+            del cords[to_go]
 
-em.show_map(field_mat)
 
+border_find(interest_calculation(field_mat,coefficient_mat),field_mat,(3,5),1)
 
+# print(coefficient_mat1)
+# robot_position = [4, 3]
+# robot_orientation = 1
+# em.reveal_2x3(robot_position, robot_orientation, field_mat, visible_mat)
+# print(visible_mat)
+# # print(weight_mat)
+# em.show_map(visible_mat, robot_position, robot_orientation)
+# em.turn_robot(1)
+# em.reveal_2x3(robot_position, robot_orientation, field_mat, visible_mat)
+# em.show_map(visible_mat, robot_position, robot_orientation)
